c22dcd52c462ffff4a1f96622c7db417429ff694
Damian Stasiak <poczta@damianstasiak.pl>
2022-10-04 19:40:51 +0200

    some commit, started reading config = checking in config file for source dir

--- docu_links.py
+++ docu_links.py
@@ -1,4 +1,13 @@
-
+from msilib.schema import Error
+import script_config as cfg
 
 
 print ("Starting doculinks...")
+
+
+try:
+    cfg.read_start_folder()
+except cfg.ConfigError as err:
+    print("Fatal error: cannot proceed")
+    print (err)
+    print("Forced stop...")
--- script_config.py
+++ script_config.py
@@ -1,1 +1,31 @@
 #this is config file for script docu_links.py
+
+import preprocessing
+from os.path import exists
+from os.path import isdir
+
+print ("Loading configuration...")
+
+start_folder = r'c:\Users\poczt\Dropbox\Dokumenty\KSIAZKI_EBOOKI\prawo-gumiakow.pdf' #write starting folder here, r' = means raw string
+
+class ConfigError(Exception):
+    pass
+
+def read_start_folder():
+    print ("read_start_folder()")
+    
+    #Check if start_folder is correct Path
+    if(exists(start_folder)):
+        print("Success: resource: "+start_folder+" exists")
+        print("Continuing...")
+    else:
+        raise ConfigError("Resource: "+start_folder+" does not exists")
+    
+    #Check if start_folder is directory
+    if(isdir(start_folder)):
+        print("Another Success: resource: +"+start_folder+" is directory")
+    else:
+        raise ConfigError("Resource: "+start_folder+" is not directory")
+
+    print("Successfully located start_folder")
+
